db1c1c32924741c16796163451ebfefcf6a13873
ravindrap52 <ravindrapallikonda@gmail.com>
2024-11-24 15:52:20 +0100

    build(linting): ignoring the dist directory while linting

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,11 @@ import tseslint from 'typescript-eslint';
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: globals.browser } },
+  {
+    ignores: [
+      'dist/**/*', // ignore all contents in and under `build/` directory but not the `build/` directory itself
+    ],
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
